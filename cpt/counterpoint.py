39052633b20cc6684f81747ec68cfa68b4d36ad6
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2020-09-29 01:28:07 -0700

    finish counterpoint algorithm

--- cpt/counterpoint.py
+++ cpt/counterpoint.py
@@ -30,7 +30,6 @@ def isInKey(scale, note):
 
 
 def contraryMotionScore(cfNotes, path):
-    # cfNotes = cfLine.flat.getElementsByClass(note.Note)
     assert len(cfNotes) == len(path) # for first species, cf and cp need to have the same number of notes
     rawScore = 0
     # for each two consecutive intervals that are not equal, add 1 to raw score
@@ -43,8 +42,38 @@ def contraryMotionScore(cfNotes, path):
     return normalizedScore
 
 
-def followSkipOppositeDirectionScore(path):
-    pass
+def perfectIntervalScore(cfNotes, path):
+    "Returns score of (num of imperfect intervals) / (num of notes - 2)"
+    assert len(cfNotes) == len(path) # for first species, cf and cp need to have the same number of notes
+    rawScore = 0
+    # for each two consecutive intervals that are not equal, add 1 to raw score
+    for i in range(len(cfNotes)):
+        currInterval = interval.Interval(cfNotes[i], path[i])
+        if currInterval.name not in ('P1', 'P4', 'P5', 'P8'):
+            rawScore += 1
+    normalizedScore = rawScore / (len(cfNotes) - 2)
+    return normalizedScore
+
+
+def followSkipScore(path):
+    "Returns score of (num skips followed by op dir + 0.5 num skips followed by same dir step) / (number of skips)"
+    previousSkip = 0
+    numSkipsFollowedOp = 0 # number of skips followed by opposite direction
+    numSkipsFollowedSame = 0 # number of skips followed by same direction skip
+    numSkips = 0 # number of skips
+    for i in range(len(path) - 1):
+        semitones = interval.Interval(path[i], path[i + 1]).semitones
+        if previousSkip != 0:
+            if previousSkip * semitones < 0:
+                numSkipsFollowedOp += 1
+            elif semitones == 1 or semitones == 2:
+                numSkipsFollowedSame += 0.5
+        if semitones > 4:
+            numSkips += 1
+            previousSkip = semitones
+        else:
+            previousSkip = 0
+    return 1.0 if numSkips == 0 else (numSkipsFollowedOp + numSkipsFollowedSame) / numSkips        
 
 
 def determineFirstSpecies(cfLine, cfScale):
@@ -130,28 +159,87 @@ def determineFirstSpecies(cfLine, cfScale):
             pathsFound[measurenumber][(noten.name + str(noten.octave))] = paths
     
     cfNotes = cfLine.flat.getElementsByClass(note.Note)
+    print('[' + ', '.join([(notem.name + str(notem.octave)) for notem in cfNotes]) + ']')
 
     pathsToBeScored = []
+    highestScore = 0
+    highestScoringPaths = []
     for pathlist in pathsFound[len(cpNotePoss) - 1].values():
         for path in pathlist:
             if validTwoSkips(path) and noDissonantOutline(path) and hasValidClimax(path) and noFourParallels(cfNotes, path):
                 pathsToBeScored.append(path)
-                print('[' + ', '.join([notem.name for notem in path]) + ']')
+                # calc score vector [contrary motion, follow skip opp dir, climax near middle]
+                weights = [0.8, 0.9, 0.6, 0.7]
+                scores = [contraryMotionScore(cfNotes, path), followSkipScore(path), round(climaxScore(path), 2), round(perfectIntervalScore(cfNotes, path), 2)]
+                embedding = [weights[i] * scores[i] for i in range(len(scores))]
+                finalScore = round(sum(embedding), 2)
+                print('[' + ', '.join([(notem.name + str(notem.octave)) for notem in path]) + '], scores: ' + str(scores) + ', final score: ' + str(finalScore))
+
+                if finalScore > highestScore:
+                    highestScore = finalScore
+                    highestScoringPaths = [path]
+                elif finalScore == highestScore:
+                    highestScoringPaths.append(path)
+                
+            # elif not noFourParallels(cfNotes, path):
+                # print('fOUR PARALLESL: [' + ', '.join([(notem.name + str(notem.octave)) for notem in path]) + ']')
+    
+    for path in highestScoringPaths:
+        print('[' + ', '.join([(notem.name + str(notem.octave)) for notem in path]) + ']')
+    print('final score: ' + str(highestScore))
+    print('[' + ', '.join([(notem.name + str(notem.octave)) for notem in cfNotes]) + ']')
+            
 
 
 def validTwoSkips(path):
+    for i in range(len(path)-2):
+        thisInt = interval.Interval(path[i], path[i+1])
+        if abs(thisInt.semitones) > 4:
+            nextInt = interval.Interval(path[i+1], path[i+2])
+            if abs(nextInt.semitones) > 4 and thisInt.semitones * nextInt.semitones > 0:
+                # this is the code for 2 skips in SAME direction
+                # check if second skip is smaller than first
+                if abs(nextInt.semitones) > abs(thisInt.semitones):
+                    return False
+                # check if combined interval is consonant
+                if interval.Interval(path[i], path[i+2]).name not in ('m6', 'M6', 'P8', 'm9', 'M9', 'm10', 'M10'):
+                    return False
     return True
 
+
 def noDissonantOutline(path):
+    localminmax = 0
+    localint = interval.Interval(path[0], path[1])
+    direction = 0 if localint.semitones == 0 else localint.semitones / abs(localint.semitones)
+    for i in range(1, len(path) - 1):
+        localint = interval.Interval(path[i], path[i + 1])
+        newdirection = 0 if localint.semitones == 0 else localint.semitones / abs(localint.semitones)
+        if direction != newdirection:
+            overarchingint = interval.Interval(path[localminmax], path[i])
+            if overarchingint.semitones in (6, 10, 11):
+                return False
+            localminmax = i
+            direction = newdirection
     return True
+
 
 def findClimax(path):
     climax = path[0]
     for n in path[1:]:
-        testint = interval.Interval(n, climax)
+        testint = interval.Interval(climax, n)
         if testint.semitones > 0:
             climax = n
-    return climax
+    return climax # returns note
+
+
+def findClimaxIndex(path):
+    climax = 0
+    for i in range(1, len(path)):
+        testint = interval.Interval(path[climax], path[i])
+        if testint.semitones > 0:
+            climax = i
+    return climax # returns index of climax
+
 
 def hasValidClimax(path):
     climax = findClimax(path)
@@ -161,17 +249,29 @@ def hasValidClimax(path):
             climaxCount += 1
     return path[0] != climax and path[-1] != climax and climaxCount == 1
 
+
+#TODO: doesn't work, but not necessary (use non-alt function)
 def hasValidClimaxAlt(path):
     climax = path[0]
     climaxCount = 1
     for n in path[1:]:
-        testint = interval.Interval(n, climax)
+        testint = interval.Interval(climax, n)
         if testint.semitones > 0:
             climax = n
             climaxCount = 1
         elif testint.semitones == 0:
             climaxCount += 1
     return True
+
+
+def climaxScore(path):
+    '''This function returns a score of how close the climax is to the center
+        of the path, according to the following formula (latex):
+        $$y = \left(1-\left(\frac{x-3.5}{3.5}\right)^2\right)^{0.8}$$'''
+    climaxIndex = findClimaxIndex(path)
+    graphshift = (len(path) - 1) / 2
+    return (1 - (((climaxIndex - graphshift) / graphshift) ** 2)) ** 0.8
+
 
 def noFourParallels(cfNotes, path):
     numParallels = 0
@@ -180,7 +280,7 @@ def noFourParallels(cfNotes, path):
         nextInt = interval.Interval(cfNotes[i+1], path[i+1])
         if currInt.semitones == nextInt.semitones:
             numParallels += 1
-            if numParllels > 3:
+            if numParallels > 3:
                 return False
         else:
             numParallels = 0
@@ -210,11 +310,12 @@ def noFourParallels(cfNotes, path):
     return cp
     '''
 
+
 if __name__ == "__main__":
 
     cmajorscale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
-    source = converter.parse('cpt/cf3.mxl')
+    source = converter.parse('cpt/cf4.mxl')
     # source.show('text')
 
     # cantus firmus
