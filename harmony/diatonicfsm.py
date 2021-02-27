3a135c1defca85e0e2062358bce35d5e57a831ba
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2021-02-27 01:39:35 -0800

    grouped chords into D and S

--- harmony/diatonicfsm.py
+++ harmony/diatonicfsm.py
@@ -1,21 +1,21 @@
 from enum import Enum
 
-class ChordRN(Enum):
-    # Chord Roman Numerals
-    I = 'I'
-    ii = 'ii'
-    iii = 'iii'
-    IV = 'IV'
-    V = 'V'
-    vi = 'vi'
-    viio = 'viio'
+# class ChordRN(Enum):
+#     # Chord Roman Numerals
+#     I = 'I'
+#     ii = 'ii'
+#     iii = 'iii'
+#     IV = 'IV'
+#     V = 'V'
+#     vi = 'vi'
+#     viio = 'viio'
 
 # class ChordK(key):
 
 #     def __init__(self, key):
 #         for chordsymbol in ChordRN
 
-
+'''
 classicalrules = {
     ChordRN.I: [ChordRN.I, ChordRN.ii, ChordRN.iii, ChordRN.IV, ChordRN.V, ChordRN.vi, ChordRN.viio],
     ChordRN.ii: [ChordRN.ii, ChordRN.IV, ChordRN.V, ChordRN.viio],
@@ -25,7 +25,17 @@ classicalrules = {
     ChordRN.vi: [ChordRN.ii, ChordRN.IV, ChordRN.vi],
     ChordRN.viio: [ChordRN.I, ChordRN.V, ChordRN.viio]
 }
+'''
 
+classicalrules = {
+    'I': ['I', 'iii', 'S', 'D', 'vi'],
+    'iii': ['iii', 'S', 'vi'],
+    'S': ['I', 'S', 'D'],
+    'D': ['I', 'D', 'vi'],
+    'vi': ['S', 'vi']
+}
+
+'''
 chordsCMajortoRN = {
     'C': ChordRN.I,
     'D': ChordRN.ii,
@@ -35,14 +45,15 @@ chordsCMajortoRN = {
     'A': ChordRN.vi,
     'B': ChordRN.viio
 }
+'''
 
-def check(path, chorddict=classicalrules):
+def check(pathRN, chorddict=classicalrules):
     # check if a path of chords (ex. path = ['C', 'D', 'G', 'C']) follows the rules of the FSM
-    pathRN = [chordsCMajortoRN[c] for c in path]
+    # pathRN = [chordsCMajortoRN[c] for c in path]
     # examplePathRN = [ChordRN.I, ChordRN.vi, ChordRN.IV, ChordRN.V, ChordRN.I]
-    if pathRN[0] != ChordRN.I:
+    if pathRN[0] != 'I':
         return False
-    for i in range(len(path) - 1):
+    for i in range(len(pathRN) - 1):
         if pathRN[i + 1] not in chorddict[pathRN[i]]:
             return False
     return True
--- harmony/hybrid.py
+++ harmony/hybrid.py
@@ -1,9 +1,51 @@
 from music21 import *
 import diatonicfsm
 
-def findChords(melody, scale): # list(note) melody, list(lettername) scale
-    chordlist = [] # should contain lists of chords
 
+'''
+I: 1 3 5
+iii: 3 5 7
+S: 1 2 4 6
+D: 2 4 5 7
+vi: 1 3 6
+'''
+
+chordPossDict = {
+    1 : ['I', 'S', 'vi'],
+    2 : ['S', 'D'],
+    3 : ['I', 'iii', 'vi'],
+    4 : ['S', 'D'],
+    5 : ['I', 'iii', 'D'],
+    6 : ['S', 'vi'],
+    7 : ['iii', 'D']
+}
+
+chromaticScale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+
+def createScaleDict(tonicname):
+    tonic = chromaticScale.index(tonicname)
+    scaleDict = {
+        chromaticScale[tonic] : 1,
+        chromaticScale[(tonic + 1) % 12] : 1.5,
+        chromaticScale[(tonic + 2) % 12] : 2,
+        chromaticScale[(tonic + 3) % 12] : 2.5,
+        chromaticScale[(tonic + 4) % 12] : 3,
+        chromaticScale[(tonic + 5) % 12] : 4,
+        chromaticScale[(tonic + 6) % 12] : 4.5,
+        chromaticScale[(tonic + 7) % 12] : 5,
+        chromaticScale[(tonic + 8) % 12] : 5.5,
+        chromaticScale[(tonic + 9) % 12] : 6,
+        chromaticScale[(tonic + 10) % 12] : 6.5,
+        chromaticScale[(tonic + 11) % 12] : 7
+    }
+    return scaleDict
+
+
+def findChords(melody, tonic): # list(note) melody, str tonic
+    
+    chordlist = [] # should contain lists of chords
+    '''
     tonic = scale[0]
     chordscale = []
     for i in range(7):
@@ -12,11 +54,26 @@ def findChords(melody, scale): # list(note) melody, list(lettername) scale
         # print(chordscale[i])
         # print(chordscale[i][0].root(), end=' ')
         # print(chordscale[i][0].commonName)
-
+    
     # chordlist is a list that corresponds to each note in the melody. each item of the list is all the possible chords that can go with that melody note
     # assume start on tonic
     chordlist.append([chordscale[0][2]])
-    
+    '''
+
+
+    scaleDict = createScaleDict(tonic)
+
+    # melodyNums = []
+    for i in range(0, len(melody)):
+        localMelodyNum = scaleDict[melody[i]]
+        # melodyNums.append(localMelodyNum)
+        if (i == 0) or (i == len(melody) - 1):
+            chordlist.append(['I'])
+        else:
+            chordlist.append(chordPossDict[localMelodyNum])
+        
+
+    '''
     # generate possibilities for chords in between
     for i in range(1, len(melody) - 1):
         chordpossibilities = []
@@ -27,7 +84,7 @@ def findChords(melody, scale): # list(note) melody, list(lettername) scale
 
     # assume end on tonic
     chordlist.append([chordscale[0][2]])
-
+    '''
 
     # create list of possible chord progressions
     paths = [[]]
@@ -45,6 +102,7 @@ def findChords(melody, scale): # list(note) melody, list(lettername) scale
     for path in paths:
         if diatonicfsm.check(path):
             validpaths.append(path)
+            print(i, end=' ')
             print(path)
             i+=1
     print(i)
@@ -58,7 +116,7 @@ def findChords(melody, scale): # list(note) melody, list(lettername) scale
 
 if __name__ == "__main__":
 
-    cmajorscale = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4']
+    # cmajorscale = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4']
 
     source = converter.parse('chorale/mel221.mxl')
     # source.show('text')
@@ -69,12 +127,12 @@ if __name__ == "__main__":
     melnotes = []
     for elem in mel.flat.getElementsByClass(note.Note):
         # print(elem)
-        melnotes.append(elem)
+        melnotes.append(elem.name)
     
     print('melody:' + str(melnotes))
 
     # write bass line
-    chords = findChords(melnotes, cmajorscale)
+    chords = findChords(melnotes, 'C')
 
     '''
 
